f1b2e6c0d65ab520343697dde31a88d877e4a257
Karen Khalifah <karen.khalifah@gmail.com>
2019-05-14 21:42:27 -0400

    constructors and objects

--- budgety_App/app.js
+++ budgety_App/app.js
@@ -1,11 +1,33 @@
 var budgetController = (function(){
     //wrapped in IFFE
+   var Expense = function(id, description, value) {
+       this.id = id;
+       this.description = description;
+       this.value = value;
+   };
     
-   //some code
+    var Income = function(id, description, value) {
+       this.id = id;
+       this.description = description;
+       this.value = value;
+   };
+   
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }       
+    }
+     
     
 })();
 
 
+//UI CONTROLLER
 var UIController = (function() {
     //storing all strings together in one olace
     var DOMstrings = {
@@ -30,29 +52,38 @@ var UIController = (function() {
     
 })();
 
-
+//CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
     
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function() {
+         var DOM = UICtrl.getDOMstrings();
+            document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+            document.addEventListener('keypress', function(event){
+                                    //older browsers
+            if(event.keyCode === 13 || event.which===13){
+            ctrlAddItem();
+        }
+    });
+    };
     
     var ctrlAddItem = function(){
         // 1. get filled input data
         var input = UICtrl.getInput();
-        console.log(input);
         
        // 2. add the item to the budget controller
        //3. add the item to the budget controller
        //4. calc the budget
        //5. diaplay the budget on the UI
         
-    }
-   document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    };
     
-    document.addEventListener('keypress', function(event){
-                                //older browsers
-        if(event.keyCode === 13 || event.which===13){
-           ctrlAddItem();
+    return {
+        init: function(){
+            console.log("app has started");
+            setupEventListeners();
         }
-    });
+    }
     
 })(budgetController, UIController);
+
+controller.init();
