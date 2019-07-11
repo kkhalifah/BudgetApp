ba133fd2ca9dc5f1eeb7328a7bde99012964fc3e
Karen Khalifah <karen.khalifah@gmail.com>
2019-07-10 22:19:39 -0400

    added dymanic date

--- budgety_App/app.js
+++ budgety_App/app.js
@@ -155,7 +155,8 @@ var UIController = (function () {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
     
      var formatNumber = function(num, type) {
@@ -262,6 +263,15 @@ var UIController = (function () {
                 
             });
         },
+        
+        displayMonth: function(){
+            var now, year, month, months;
+          now = new Date();
+            months= ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
 
         getDOMstrings: function () {
             return DOMstrings;
@@ -353,6 +363,7 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     return {
         init: function () {
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
